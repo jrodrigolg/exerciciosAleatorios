6b43796e22b9735d1d89f1ecdcb046a9dc2c9fb8
Rodrigo Gadelha <jrodrigolg@gmail.com>
2020-06-25 18:14:23 -0300

    correçao

--- src/exercicios/Ex1124.java
+++ src/exercicios/Ex1124.java
@@ -1,5 +1,5 @@
 package exercicios;
-
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1124
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,19 +9,31 @@ public class Ex1124 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		int L=1, C=1, R1=1, R2=1;
+		boolean x = false;
 		while(true) {
 			L=sc.nextInt();
 			C=sc.nextInt();
 			R1=sc.nextInt();
 			R2=sc.nextInt();
+			
 			if(L==0 && C==0 && R1==0 && R2 ==0) {
 				break;
 			}
-			
-			if(((2*R1+2*R2)<=L && 2*R1<=C && 2*R2<=C) || ((2*R1+2*R2)<=C && 2*R1<=L && 2*R2<=L)) {
-				System.out.println("S");
+			if(2*R1<=L && 2*R2<=L && 2*R1<=C && 2*R2<=C) {
+				if((2*R1+2*R2)<=L || (2*R1+2*R2)<=C) {
+					x = true;
+				}
+				else if(teste(R1, R2, L, C)){
+					x = true;
+				}
+				else {
+					x = false;
+				}
 			}
-			else if(teste(R1, R2, L, C)){
+			else{
+				x = false;
+			}
+			if(x) {
 				System.out.println("S");
 			}
 			else {
@@ -29,12 +41,13 @@ public class Ex1124 {
 			}
 		}
 		sc.close();
+		
 	}
 	public static boolean teste(int r1,int r2, int l, int c) {
 		double x, y;
 		x = l-r1-r2;
 		y = Math.sqrt(Math.pow((r1+r2), 2)-(x*x));
 		y+=r1+r2;
-		return y<=c && 2*r1<=l && 2*r2<=l && 2*r1<=c && 2*r2<=c;
+		return y<=c;
 	}
 }
